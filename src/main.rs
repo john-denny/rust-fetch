84ebbc58589e6f374499ba6c0a0f421722c1236b
John Denny <johndenny116@gmail.com>
2023-12-30 15:45:21 +0000

    Added name & Hostname feature. Reordered Code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,46 @@ use regex::Regex;
 use std::fs;
 use std::process::Command;
 
+fn get_title() -> String {
+    let username = Command::new("whoami")
+        .output()
+        .expect("Unable to get username");
+    let username = String::from_utf8_lossy(&username.stdout).trim().to_string();
+
+    let hostname = Command::new("hostname")
+        .output()
+        .expect("Unable to get hostname");
+    let hostname = String::from_utf8_lossy(&hostname.stdout).trim().to_string();
+
+    return format!("{}@{}", username, hostname);
+}
+
+// Function to get the current date and time
+fn get_date() -> String {
+    let output = Command::new("date")
+        .output()
+        .expect("Unable to get Time Information");
+    return String::from_utf8_lossy(&output.stdout).trim().to_string();
+}
+
+fn get_pretty_name() -> String {
+    // Open the OS file
+    let os_file: String =
+        fs::read_to_string("/etc/os-release").expect("Unable to read OS Details from OS file");
+
+    // Get the Os's "Pretty Name"
+    let get_pretty_name_re = Regex::new(r#"PRETTY_NAME="([^"]+)""#).unwrap();
+
+    let mut pretty_name: String = String::new();
+
+    if let Some(captures) = get_pretty_name_re.captures(&os_file) {
+        if let Some(matched) = captures.get(1) {
+            pretty_name = matched.as_str().to_string();
+        }
+    }
+    return pretty_name;
+}
+
 // Function to get the devices uptime
 fn get_uptime() -> Result<String, std::io::Error> {
     let output = Command::new("uptime").arg("-p").output()?;
@@ -17,53 +57,33 @@ fn get_uptime() -> Result<String, std::io::Error> {
     }
 }
 
-// Function to get the current date and time
-fn get_date() -> String {
-    let output = Command::new("date")
-        .output()
-        .expect("Unable to get Time Information");
-    return String::from_utf8_lossy(&output.stdout).trim().to_string();
-}
-
 fn main() {
-    // Open the OS file
-    let os_file: String =
-        fs::read_to_string("/etc/os-release").expect("Unable to read OS Details from OS file");
-
-    // Get the Os's "Pretty Name"
-    let get_pretty_name_re = Regex::new(r#"PRETTY_NAME="([^"]+)""#).unwrap();
-
-    let mut pretty_name: String = String::new();
-
-    if let Some(captures) = get_pretty_name_re.captures(&os_file) {
-        if let Some(matched) = captures.get(1) {
-            pretty_name = matched.as_str().to_string();
-        }
-    }
-
     // Get the Computer's model name
-    let mut model: String = fs::read_to_string("/sys/devices/virtual/dmi/id/product_name")
+    let mut model_name: String = fs::read_to_string("/sys/devices/virtual/dmi/id/product_name")
         .expect("Unable to read Computer's Model from file");
 
-    model.pop(); // Remove the newline character from the model number string
+    model_name.pop(); // Remove the newline character from the model number string
 
     // Slices the uptime
     let uptime = get_uptime().expect("Failed to get uptime");
     let uptime = &uptime[3..];
 
+    let title = get_title();
     let tux = format!(
         r#"
      .---.      {}
-    /     \     OS: {}
-    \.@-@./     HOST: {}
-    /`\_/`\     Uptime: {}
-   //  _  \\    
-  | \     )|_     
+    /     \     {}
+    \.@-@./     {}
+    /`\_/`\     OS: {}
+   //  _  \\    HOST: {}
+  | \     )|_   Uptime: {}  
  /`\_`>  <_/ \     
  \__/'---'\__/"#,
+        title,
+        "-".repeat(title.chars().count() + 1), // Makes the bar of ---s
         get_date(),
-        pretty_name,
-        model,
+        get_pretty_name(),
+        model_name,
         uptime,
     );
 
